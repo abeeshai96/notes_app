60bb2c42c380ac1b72bf3bff95b33fa5bbf3eca7
Abeesh A I <abeeshai96@gmail.com>
2021-04-06 21:28:37 +0530

    UI Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NotesStartComponent } from './home/notes-start/notes-start.component';
+import { NotesListComponent } from './home/notes-list/notes-list.component';
+import { NotesEditComponent } from './home/notes-edit/notes-edit.component';
+import { NotesDetailComponent } from './home/notes-detail/notes-detail.component';
+import { NotesItemComponent } from './home/notes-list/notes-item/notes-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotesStartComponent,
+    NotesListComponent,
+    NotesEditComponent,
+    NotesDetailComponent,
+    NotesItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/notes-list/notes-item/notes-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-item',
+  templateUrl: './notes-item.component.html',
+  styleUrls: ['./notes-item.component.css'],
+})
+export class NotesItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onViewDetail() {}
+}
