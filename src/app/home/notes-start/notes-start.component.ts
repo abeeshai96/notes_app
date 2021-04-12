8cf20398a743aa3e6e2d3c98f6db12b0d89d1981
Abeesh A I <abeeshai96@gmail.com>
2021-04-12 10:11:22 +0530

    Final Commit

--- src/app/home/notes-start/notes-start.component.ts
+++ src/app/home/notes-start/notes-start.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notes-start',
   templateUrl: './notes-start.component.html',
-  styleUrls: ['./notes-start.component.css']
+  styleUrls: ['./notes-start.component.css'],
 })
 export class NotesStartComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
